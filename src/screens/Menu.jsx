c8c817cb356dcc62fe1113da399bd22dc033634b
Akmal Adnan <akmal.a@okwave.global>
2022-12-31 01:38:34 +0800

    test

--- src/screens/Menu.jsx
+++ src/screens/Menu.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {Text, View} from 'react-native';
 
 function Menu() {
   return (
@@ -10,5 +10,3 @@ function Menu() {
 }
 
 export default Menu;
-
-const styles = StyleSheet.create({});
